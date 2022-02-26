e12960e61b2a292f7092ece4163c1d12d6066311
Arun Rathore <arunrathore0821@gmail.com>
2022-02-26 21:21:34 +0530

    add public initialiser

--- Sources/CustomPackage/CustomPackage.swift
+++ Sources/CustomPackage/CustomPackage.swift
@@ -1,6 +1,10 @@
 public struct CustomPackage {
     var text = "Hello, World!"
     
+    public init() {
+        
+    }
+    
     public func helloWorld() -> String {
         return text
         }
